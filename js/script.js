c6848b64f5c698f9175281fd4a39400efbb1bc5c
Tatyana <nikolina.tatyana@yandex.ru>
2022-08-02 02:56:29 +0500

    создание калькулятора каллорий

--- js/script.js
+++ js/script.js
@@ -296,6 +296,7 @@ window.addEventListener('DOMContentLoaded', function () {
     let slideIndex = 1;
     let offset = 0;
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -322,12 +323,81 @@ window.addEventListener('DOMContentLoaded', function () {
         slide.style.width = width;
     });
 
+    /* создание точек внизу у слайдера */
+    slider.style.position = 'relative';
+    //создание обертки внутри js для точек
+    const indicators = document.createElement('ol'),
+        dots = [];
+
+    indicators.classList.add('carousel-indicators');
+    //добавление стилей css для блока
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+
+        indicators.append(dot);
+        dots.push(dot);
+    }
+    // ф-я, к-я переводит строку в число и удаляет все нецифры
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
+    function opacityDots(arr) {
+        //перебор массива с точками и каждой задать 50% прозрачности
+        arr.forEach(dot => dot.style.opacity = '.5');
+        //при перелистовании слайдов на текущем делать 1
+        arr[slideIndex - 1].style.opacity = 1;
+    }
+    // ф-я прибавляет ноль впереди,если номер слайда меньше 10
+    // иначе просто выводит номер слайда
+    function showNumberSlides() {
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+
     next.addEventListener('click', () => {
 
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -337,20 +407,33 @@ window.addEventListener('DOMContentLoaded', function () {
             slideIndex++;
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        showNumberSlides();
 
+        opacityDots(dots);
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = deleteNotDigits(width) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            showNumberSlides();
+
+            opacityDots(dots);
+
+        });
     });
 
     prev.addEventListener('click', () => {
 
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -359,12 +442,10 @@ window.addEventListener('DOMContentLoaded', function () {
         } else {
             slideIndex--;
         }
-        
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+
+        showNumberSlides();
+
+        opacityDots(dots);
     });
 
     /* showSlides(slideIndex);
@@ -409,4 +490,145 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 
-});
+    // Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+
+    //получение знач-я из localStorage и сохр на странице
+    // если знач-е ключа 'sex' - true, то перезаписываем переменную sex
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';// иначе знач-е по умолч-ю
+        localStorage.setItem('sex', 'female');//запись в хранилище знач-я по умолчанию
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = 1.375;// иначе знач-е по умолч-ю
+        localStorage.setItem('ratio', 1.375);//запись в хранилище знач-я по умолчанию
+    }
+
+    //общая ф-я для подсчета 
+    function calcTotal() {
+        // проверка, чтобы все пункты были заполнены
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+
+    }
+    calcTotal();
+
+    //ф-я для инициализации полученных данных из хранилища и вывода на стр
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);//удаляем у всех эл-нтов класс актив
+            //проверка, если у эл-нта знач-е id совпадает со знач-ем в храниище, то
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);// добавляем класс актив
+            }
+
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);// добавляем класс актив
+            }
+        });
+    }
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+
+
+    //ф-я, к-я будет получать инфу со статических блоков
+
+    function getStaticInformation(selector, activeClass) {
+        // поиск на стр всех эл-нтов div у родителя parentSelector
+        const elements = document.querySelectorAll(selector);
+        //делегирование в этом случае не работает, поэтому навешиваем обр.события 
+        // на каждый эл-нт отдельно
+        // с помощью метода forEach перебираем эл-нты 
+        elements.forEach((elem) => {
+            elem.addEventListener('click', (e) => {
+                //проверка на наличие атрибута data-ratio
+                if (e.target.getAttribute('data-ratio')) {
+                    // вытаскиваем знач-е data-ratio при клике и переводим в число
+                    ratio = +e.target.getAttribute('data-ratio');
+                    //запись в localStorage знач-е кофф., к-ый выбрал пользователь
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
+                } else {
+                    // иначе выбираем пол мужчина либо женщина и вытаскиваем id
+                    sex = e.target.getAttribute('id');
+                    //запись в localStorage знач-е пола, к-ый выбрал пользователь
+                    localStorage.setItem('sex', e.target.getAttribute('id'));
+                }
+
+                // перебираем весь массив с эл-нтами и удаляем у них класс актив
+
+                elements.forEach((elem) => {
+                    elem.classList.remove(activeClass);
+                });
+                //затем при клике на эл-нт добавляем класс актив
+                e.target.classList.add(activeClass);
+                //необходимо запускать ф-ю для пересчета каллорий после каждых измен-й от пользователя
+                calcTotal();
+            });
+        });
+    }
+
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    //ставим . в селекторе,если это класс
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
+
+
+    // ф-я, к-ая будет обрабатывать каждый отдельный input
+    function getDynamicInformation(selector) {
+        //поиск input по селектору
+        const input = document.querySelector(selector);
+        // навешиваем обр.события на input
+        input.addEventListener('input', () => {
+            //проверка знач-я  input на нечисло и 
+            // поле ввода подсвечивается красным, если нечисло
+            if (input.value.match(/\D/g)) {
+                input.style.border = '1px solid red';
+            } else {
+                // если пользователь изменил на цифры, то убрать рамку
+                input.style.border = 'none';
+            }
+
+
+            // проверка input на id
+            switch (input.getAttribute('id')) {
+                case 'height':
+                    // если id = height, то переменная height = знач-ю в input
+                    height = +input.value;
+                    break; // остановка
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            //необходимо запускать ф-ю для пересчета каллорий после каждых измен-й от пользователя
+            calcTotal();
+        });
+
+
+
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+}); 
